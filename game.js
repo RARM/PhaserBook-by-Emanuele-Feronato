0e8e767e19b5f8f243e45a7dfa9ddb9699151730
Rodolfo Andrés Rivas Matta <rodolfo.andres.pro@gmail.com>
2019-05-02 17:32:09 -0400

    Creating the structure of the game
    Initial commit. Creating the structure of the game. And adding the first
    Phaser code.

---
+++ game.js
@@ -0,0 +1,3 @@
+window.onload = function() { // The onload event (for the windows) fires after all objects in the DOM hierarchy have finished loading
+  var game = new Phaser.Game(); // Creating a new instance of a Phaser game
+}
