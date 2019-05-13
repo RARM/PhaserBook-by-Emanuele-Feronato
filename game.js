8d6c8f37db6737d7169a456f815db3227dff5b47
Rodolfo Andrés Rivas Matta <rodolfo.andres.pro@gmail.com>
2019-05-13 08:16:53 -0400

    Creating a Phaser Game configuration object
    Adding the gameConfig variable and passing it as an argument into the
    Phaser.Game instance.

--- game.js
+++ game.js
@@ -1,3 +1,12 @@
 window.onload = function() { // The onload event (for the windows) fires after all objects in the DOM hierarchy have finished loading
-  var game = new Phaser.Game(); // Creating a new instance of a Phaser game
+  var gameConfig = {
+    /* gameConfig: contains some game settings such as width, height and background color
+       We are also passing gameConfig as an argument into the Phaser.Game at the game variable */
+    width: 480,
+    height:  640,
+    backgroundColor: 0xff0000
+  }
+
+
+  var game = new Phaser.Game(gameConfig); // Creating a new instance of a Phaser game
 }
